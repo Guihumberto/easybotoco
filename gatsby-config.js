01c0e5f522287ed8720b9e0825b80f767e2b875e
Humberto Gui <juninho.joao@hotmail.com>
2021-06-05 14:03:38 -0300

    novo teste

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,7 +82,7 @@ module.exports = {
         background_color: `#16202c`,
         theme_color: `#16202c`,
         display: `minimal-ui`,
-        icon: `src/images/easy-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/easy-icon.png`, 
       },
     },
     `gatsby-plugin-gatsby-cloud`,
